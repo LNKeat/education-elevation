908987ca13ca833ecaa268642611e683ec9be1e9
Laura <LNKEAT@gmail.com>
2023-05-13 22:35:13 -0600

    user index & create actions working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
+    #sign-up
+
+    def create
+        user = User.create!(user_params)
+        # TODO: add user to session here
+        render json: user
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :password, :password_confirmation, :email, :donor_tier, :donations_sum)
+    end
 end
