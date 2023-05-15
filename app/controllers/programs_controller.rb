36cf1499176f1783f337a4d3cfa381b3abd8716b
Laura <LNKEAT@gmail.com>
2023-05-15 12:49:36 -0600

    Add create and delete actions to programs ctrlr

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -3,4 +3,21 @@ class ProgramsController < ApplicationController
         programs = Program.all 
         render json: programs
     end
+
+    def create
+        program = Program.create!(params_permit)
+        render json: program, status: :created
+    end
+
+    def destroy
+        program = Program.find(params[:id])
+        program.destroy
+        head :no_content
+    end
+
+    private
+
+    def params_permit
+        params.permit(:name, :description, :teacher_id, :funds_needed, :funds_raised)
+    end
 end
