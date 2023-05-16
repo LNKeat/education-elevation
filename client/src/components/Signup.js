41e56249df4532d7810134f33e423a2e44857b4d
Laura <LNKEAT@gmail.com>
2023-05-15 18:12:15 -0600

    build login/sign up forms

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
 
-function Login() {
+function Signup() {
     return (
         <Container>
             <form>
+                <div class="form-group">
+                    <label for="firstName">First Name</label>
+                    <input type="text" class="form-control" id="firstName" aria-describedby="name" placeholder="Enter first name" />
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" class="form-control" id="lastName" aria-describedby="name" placeholder="Enter last name" />
+                </div>
                 <div class="form-group">
                     <label for="email">Email address</label>
                     <input type="email" class="form-control" id="email" aria-describedby="email" placeholder="Enter email" />
@@ -14,10 +22,15 @@ function Login() {
                     <label for="password">Password</label>
                     <input type="password" class="form-control" id="password" placeholder="Password" />
                 </div>
+                <div class="form-group">
+                    <label for="password">Confirm Password</label>
+                    <input type="password" class="form-control" id="passwordConfirm" placeholder="Password" />
+                </div>
+
                 <button type="submit" class="btn" style={{ backgroundColor: "#275251", color: "#ece0cd", margin: "5px" }}>Submit</button>
             </form>
         </Container>
     )
 }
 
-export default Login
+export default Signup
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import Login from '../components/Login'
+import Signup from '../components/Signup'
 
 function Home() {
   return (
     <Container>
         <h2 style={{padding:"40px"}}>Please Log in or Sign up</h2>
         <Login />
+        <br />
+        <Signup />
     </Container>
   )
 }
