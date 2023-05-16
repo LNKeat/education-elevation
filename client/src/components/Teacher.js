3c7088c929050e4f40149e8eda8aba6190f90b31
Laura <LNKEAT@gmail.com>
2023-05-16 10:14:51 -0600

    teachers mapped

---
+++ client/src/components/Teacher.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/esm/Button';
+
+// TODO: get teachers to line up 2 cards per column
+
+function Teacher({ teacher, pic }) {
+    console.log(teacher)
+    return (
+        <Container className='flex'>
+        <Card style={{ width: '18rem', margin:"20px" }}>
+          <Card.Img variant="top" src={pic} styles={{backgroundColor:"#275251"}} />
+          <Card.Body>
+            <Card.Title>{teacher.first_name} {teacher.last_name}</Card.Title>
+            <Card.Text>
+              {teacher.bio}
+              {teacher.programs.length > 0 && <h6>Programs:</h6>}
+              <ul>
+                {teacher.programs.map((p) => <li key={p.name}>{p.name}</li>)}
+              </ul>
+            </Card.Text>
+            {/* <Button variant="primary">Go somewhere</Button> */}
+          </Card.Body>
+        </Card>
+        </Container>
+      );
+}
+
+export default Teacher
--- client/src/pages/Teachers.js
+++ client/src/pages/Teachers.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Teacher from '../components/Teacher'
+import TeacherOne from '../assets/images/Teacher_1.jpg'
+import TeacherTwo from '../assets/images/Teacher_2.jpg'
+import TeacherThree from '../assets/images/Teacher_3.jpg'
+import TeacherFour from '../assets/images/Teacher_4.jpg'
 
 //TODO: map out list of teachers with their names & bios and a list of the programs they manage
+
+const teachers_images = [TeacherOne, TeacherTwo, TeacherFour, TeacherThree]
+
 function Teachers() {
+  const [teachers, setTeachers] = useState([])
+
+  useEffect(() => {
+    fetch('/teachers')
+    .then((r) => r.json())
+    .then((data) => setTeachers(data))
+  }, [])
+  
+
   return (
-    <div>
+    <Container fluid="md">
         <h1>Teachers</h1>
-    </div>
+        {teachers.map(( (t, ind) =>
+          <Teacher key={ind} teacher={t} pic={teachers_images[ind] } />
+        ))}
+
+    </Container>
   )
 }
 
