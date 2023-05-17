d0714d8837210afff663c096531b52cc20b0414c
Laura <LNKEAT@gmail.com>
2023-05-17 10:39:46 -0600

    after submit, hide form, thank user & display info

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -8,6 +8,7 @@ function Donate({ program }) {
   const [donationSum, setDonationSum] = useState(0)
   const [initProgram, setInitProgram] = useState(program)
   const [filteredPrograms, setFilteredPrograms] = useState([])
+  const [responseData, setResponseData] = useState(null)
   const [user] = useContext(UserContext)
   const [programs] = useContext(ProgramsContext)
 
@@ -19,7 +20,7 @@ function Donate({ program }) {
     program && setFilteredPrograms(programs.filter((p) => p.id !== program.id))
   }, [user, program, programs])
 
-  function handleSelectChange(e){
+  function handleSelectChange(e) {
     const program = programs.find((p) => p.id == e.target.value)
     setInitProgram(program)
   }
@@ -27,11 +28,10 @@ function Donate({ program }) {
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log("Amount: ", amount, "program id: ", initProgram.id, "user id: ", user.id )
     fetch('/donations', {
-      method: 'POST', 
+      method: 'POST',
       headers: {
-        "Content-Type":"application/json", 
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         user_id: user.id,
@@ -39,59 +39,70 @@ function Donate({ program }) {
         amount: amount
       }),
     })
-    .then((r) => {
-      if (r.ok) {
-        r.json()
-        .then((data) => console.log(data))
-      } else {
-        r.json().then((details) => console.log("errors: ", details.errors))
-      }
-    })
+      .then((r) => {
+        if (r.ok) {
+          r.json()
+            .then((data) => setResponseData(data))
+        } else {
+          r.json().then((details) => console.log("errors: ", details.errors))
+        }
+      })
   }
 
-  
+
   return (
     <Container>
-      {user ?
-        <>
-          <h1>Donate here {user.first_name}</h1>
-
-          <form onSubmit={handleSubmit}>
-            <div className='form-group' styles={{ marginBottom: "20px" }}>
-              {/* if the user arrived at the form from program component */}
-              {program ?
+      {
+        responseData ? 
+        <Container style={{padding:"20px"}}>
+          <h5>Thank you for your donation, {responseData.user.first_name}!</h5>
+          <p>You have donated ${responseData.user.donations_sum} total to our programming and you are part of our {responseData.user.donor_tier} donor tier. Our {responseData.program.name} program now has ${responseData.program.funds_raised} of ${responseData.program.funds_needed} needed.</p>
+        </Container> :
+          <>
+            {user ?
               <>
-              <label htmlFor="Program">Program:</label>
-                <select id="program" aria-label="Choose a program" onChange={handleSelectChange}>
-                  <option value={initProgram.id}>{initProgram.name}</option>
-                  {/* {programs && filteredPrograms.map((p) => (
+                <h1>Donate here {user.first_name}</h1>
+
+                <form onSubmit={handleSubmit}>
+                  <div className='form-group' styles={{ marginBottom: "20px" }}>
+                    {/* if the user arrived at the form from program component */}
+                    {program ?
+                      <>
+                        <label htmlFor="Program">Program:</label>
+                        <select id="program" aria-label="Choose a program" onChange={handleSelectChange}>
+                          <option value={initProgram.id}>{initProgram.name}</option>
+                          {/* {programs && filteredPrograms.map((p) => (
                     <option key={p.id} value={p.id}>{p.name}</option>
                   ))} */}
-                </select>
-                </> :
+                        </select>
+                      </> :
 
-                // if the user comes from the donate page map all programs
-                <>
-                <label htmlFor="Program">Select a program:</label>
-                <select id="program" aria-label="Choose a program" onChange={handleSelectChange}>
-                  {programs && programs.map((p) => (
-                    <option key={p.id} value={p.id}>{p.name}</option>
-                  ))}
+                      // if the user comes from the donate page map all programs
+                      <>
+                        <label htmlFor="Program">Select a program:</label>
+                        <select id="program" aria-label="Choose a program" onChange={handleSelectChange}>
+                          {programs && programs.map((p) => (
+                            <option key={p.id} value={p.id}>{p.name}</option>
+                          ))}
 
-                </select>
-                </>
-              }
-            </div>
-            <div className="form-group">
-              <label htmlFor="amount">Donation Amount: </label>
-              <input type="number" className="form-control" id="amount" aria-describedby="amount" placeholder="Enter amount you'd like to donate" value={amount} onChange={(e) => setAmount(e.target.value)} />
-            </div>
+                        </select>
+                      </>
+                    }
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="amount">Donation Amount: </label>
+                    <input type="number" className="form-control" id="amount" aria-describedby="amount" placeholder="Enter amount you'd like to donate" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                  </div>
 
 
 
-            <button type="submit" className="btn" style={{ backgroundColor: "#275251", color: "#ece0cd", margin: "5px" }}>Submit</button>
-          </form>
-        </> : <a href='/' style={{ color: "#275251" }}>Please log in or create an account to donate</a>}
+                  <button type="submit" className="btn" style={{ backgroundColor: "#275251", color: "#ece0cd", margin: "5px" }}>Submit</button>
+                </form>
+              </> : <a href='/' style={{ color: "#275251" }}>Please log in or create an account to donate</a>}
+
+            {/* After donation is made:  */}
+          </>
+      }
     </Container>
   )
 }
