a0d2f8cf81d8daf175c9dcfe7da1115538b553b7
Laura <LNKEAT@gmail.com>
2023-05-16 18:35:07 -0600

    add profile information

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../App'
+import Container from 'react-bootstrap/esm/Container'
 
 // TODO: thank you message with donor name, donation tier they belong to, donation sum
 
 function Profile() {
+  const [user] = useContext(UserContext)
+
   return (
     <div>
-        <h1>Profile</h1>
+      <h1>Profile</h1>
+      {user && 
+      <Container>
+        <h6>Name: {user.first_name} {user.last_name}</h6>
+        <h6>Total donations: {user.donations_sum}</h6>
+        <h5>Thank you for your {user.donor_tier} donor status!</h5>
+      </Container>
+      }
     </div>
   )
 }
