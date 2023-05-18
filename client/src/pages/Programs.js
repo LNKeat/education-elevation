65aaa7ac0f947fd3f8631bfa7ece873481e86255
Laura <LNKEAT@gmail.com>
2023-05-17 21:51:19 -0600

    get programs to upd on Dom:delete --problemTODO

--- client/src/pages/Programs.js
+++ client/src/pages/Programs.js
@@ -12,22 +12,31 @@ function Programs() {
 
   useEffect(() => {
     programs && setFilteredPrograms(programs)
-  }, [])
+  }, [programs])
 
-  function handleDeleteProgram(program){
-    console.log(program.id)
+
+  function handleDeleteProgram(program) {
     fetch(`/programs/${program.id}`, {
       method: 'DELETE',
-    }).then((r) => r.json())
-    .then(() => {
-      programs && setFilteredPrograms(programs.filter((p) => p.id !== program.id))
+    }).then((r) => {
+      if (r.ok) {
+        console.log('delete worked')
+        setFilteredPrograms(programs.filter((p) => p.id !== program.id))
+      } else {
+        r.json().then((details) => setDeleteErrors(details.errors))
+      }
     })
   }
 
   return (
     <Container style={{ padding: "20px" }}>
-        <h1>Programs</h1>
-        {programs && programs.map((p) => <Program key={p.id} program={p} programs={programs} handleDeleteProgram={handleDeleteProgram} />)}
+      <h1>Programs</h1>
+      {programs ? filteredPrograms.map((p) => <Program key={p.id} program={p} programs={programs} handleDeleteProgram={handleDeleteProgram} />) : <>not working</>}
+      <ul style={{ color: "red" }}>
+        {deleteErrors.map((error, ind) => (
+          <li key={ind}>{error}</li>
+        ))}
+      </ul>
     </Container>
   )
 }
