b1d55a7946900aa70ffddf55f955470dff0e323f
Laura <LNKEAT@gmail.com>
2023-05-20 21:25:05 -0600

    typo change, delete file

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -3,6 +3,6 @@ class DonationMailer < ApplicationMailer
   def donation_email
     donation = params[:donation]
     @user = donation.user
-    mail(to: @user.email, subject: 'Thank you for your donation')
+    mail(to: @user.email, subject: 'Thank you for your donation!')
   end
 end
